bfae78b08a8123b9efb067740096656da2cfa05c
SHEILA-DIAZ <sheila.diaz@tecsup.edu.pe>
2026-04-29 17:18:26 -0500

    Create movie model

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Movie
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "release_year",
+        "duration_minutes",
+        "is_available",
+    )
+    list_filter = ("is_available", "release_year")
+    search_fields = ("title",)
+    ordering = ("id",)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=150)
+    synopsis = models.TextField()
+    release_year = models.PositiveIntegerField()
+    duration_minutes = models.PositiveIntegerField()
+    is_available = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return self.title
